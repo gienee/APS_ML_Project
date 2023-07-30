9c7bd4fae98bdce5865f39b169c2728550a70261
gienee <nikitathakur9938@gmail.com>
2023-07-30 23:33:22 +0530

    updated pipeline folder and main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-import pymongo
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys,os
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+def test_logger_and_exception():
+     try:
+          logging.info("Starting the test_logger_and_exception")
+          result=3/0
+          print(result)
+          logging.info("Stoping the test_logger_and_exception")
+     except Exception as e:
+          logging.debug(str(e))
+          raise SensorException(e,sys)
 
-# Database Name
-dataBase = client["labDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': '-',
-     'product': '-',
-     'Offered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+if __name__=="__main__":
+     try:
+          test_logger_and_exception()
+     except Exception as e:
+          print(e)
