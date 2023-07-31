c9af400954a3efb46f1d0b8d48f897982502484f
gienee <nikitathakur9938@gmail.com>
2023-07-31 18:25:14 +0530

    Dataingestionconfig and Dataingestion file updated

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
-import sys,os
+from sensor.utils import get_collection_as_dataframe
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
 
-def test_logger_and_exception():
-     try:
-          logging.info("Starting the test_logger_and_exception")
-          result=3/0
-          print(result)
-          logging.info("Stoping the test_logger_and_exception")
-     except Exception as e:
-          logging.debug(str(e))
-          raise SensorException(e,sys)
 
 if __name__=="__main__":
      try:
-          test_logger_and_exception()
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
